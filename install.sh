c7bc662eab0eb331270d20d59ad789d661f2ef18
kdav5758 <sebastiancadavid5758@gmail.com>
2021-03-27 09:05:00 -0500

    Added contents to the installation file

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+sudo chmod 775 /usr/local/bin/
+cd /usr/local/bin/
+git clone https://github.com/kdav5758/.dotfiles/tree/dev
+sudo chmod 775 /usr/local/bin/MiniPrompt/mini_prompt.sh; sudo chmod 775 /usr/local/bin/MiniPrompt/on_da_fly.sh
